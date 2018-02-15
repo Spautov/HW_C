b491487924984ddd82e5dcd41070d8934c7b661c
Spautov <Pautovs@mail.ru>
2018-02-15 10:02:18 +0300

    Add files via upload

---
+++ HW_1_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Every \n"
+    <<"\thunter \n"
+    <<"\t\twants \n"
+    <<"\t\t\tto know \n"
+    <<"\t\t\t\twhere \n"
+    <<"\t\t\t\t\tpheasant sits" << std::endl;
+    return 0;
+}
