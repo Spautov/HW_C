b92c2582d7d43ff82fa069efc1b7961ca80254c4
Spautov <Pautovs@mail.ru>
2018-02-15 09:52:32 +0300

    Add files via upload

---
+++ HW_1_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Leaves are floating \n"
+              <<"softly down; \n"
+              <<"Some are red and \n"
+              <<"some are brown. \n"
+              <<"The wind goes \"swish\" \n"
+              <<"through the air; \n"
+              <<"When you look back, \n"
+              <<"there are no leaves there. \n \n"
+              <<"\tThe author is unknown \n \n";
+
+    return 0;
+}
